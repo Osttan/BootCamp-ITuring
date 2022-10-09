f5042a2e6496c06ee051b7ce24d1c6402e146258
Andre Ostan <andre.ostan@gmail.com>
2022-10-09 17:06:02 -0300

    Adicionando a prática 5

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,9 @@
+const nome = "Andre",
+  email = "andre.ostan@gmail.com",
+  profissao = "servidor público",
+  idade = 34,
+  lugar = "London";
+
+console.log(
+  `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+);
