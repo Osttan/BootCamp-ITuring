456c4b1e81e114b70e4926da1451ae23d6c4a30a
Andre Ostan <andre.ostan@gmail.com>
2022-10-09 16:58:51 -0300

    Adicionando o arquivo de prática 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+let nome = "Andre",
+  email = "andre.ostan@gmail.com",
+  profissao = "servidor público",
+  idade = 34,
+  lugar = "London";
+
+console.log(typeof nome == "string");
+console.log(typeof email == "string");
+console.log(typeof profissao == "string");
+console.log(typeof idade == "string");
+console.log(typeof lugar == "string");
