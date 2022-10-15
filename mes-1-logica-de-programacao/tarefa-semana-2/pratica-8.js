2aa3f58da66640f0d5098aa06a9e6c70f6c27a37
Andre Ostan <andre.ostan@gmail.com>
2022-10-15 15:49:28 -0300

    Prática 8 Concluída

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,9 @@
+function ehPar(numero) {
+  if (numero % 2 === 0) {
+    console.log(`O número ${numero} é par`);
+  } else {
+    console.log(`O número ${numero} é ímpar`);
+  }
+}
+
+ehPar(1577846213);
